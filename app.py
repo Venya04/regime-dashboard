7fe673bee497a68d1795d360d3b40bd6ded8b534
Venya <154803459+Venya04@users.noreply.github.com>
2025-06-17 17:39:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -214,7 +214,7 @@ with left_col:
         <style>
             .left-section-title {
                 font-family: Georgia, serif;
-                font-size: 1.1rem;
+                font-size: 0.1rem;
                 font-weight: bold;
                 text-transform: uppercase;
                 margin-bottom: 10px;
