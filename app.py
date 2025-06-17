fa4f3a6e41010af94d62654c0cc1d5e104c9f0d1
Venya <154803459+Venya04@users.noreply.github.com>
2025-06-17 18:37:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -238,7 +238,7 @@ with left_col:
                     "stablecoins": "#3A3A3A",
                     "cash": "#5C5149",
                     "crypto": "#2F4F4F",
-                    "commodities": "#a87a01",
+                    "commodities": "#ad8e3a",
                 }
             )
 
