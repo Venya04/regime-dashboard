945a53089f28fdb8195b79604452ed4649a574a3
Venya <154803459+Venya04@users.noreply.github.com>
2025-06-18 17:49:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -401,7 +401,7 @@ if not perf_df.empty:
     perf_fig.update_traces(line=dict(width=3), marker=dict(size=6))
     perf_fig.update_layout(
         height=350,
-        width=1100,
+        width=900,
         margin=dict(l=20, r=20, t=10, b=20),
         autosize=True,
         paper_bgcolor='rgba(0,0,0,0)',
