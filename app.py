4f6e6d6ab858b3bbf227fdeb33e6e7dc271dd6e2
Venya <154803459+Venya04@users.noreply.github.com>
2025-06-19 12:36:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ button_label = "📘 Open Guide" if not st.session_state.show_guide else "❌ Cl
 guide_button_html = f"""
 <div style="
     position: absolute;
-    top: 110px;
+    top: 60px;
     left: 20px;
     z-index: 1000;
 ">
