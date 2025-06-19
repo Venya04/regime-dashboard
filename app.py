7b87756c31a28e30bd68e2c73e189a03972a93fd
Venya <154803459+Venya04@users.noreply.github.com>
2025-06-19 09:36:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -507,11 +507,11 @@ if not perf_df.empty:
     """, height=400)
 
 # Hide Streamlit menu and footer
-st.markdown("""
-    <style>
-        #MainMenu {visibility: hidden;}
-        footer {visibility: hidden;}
-        header {visibility: hidden;}
-    </style>
-""", unsafe_allow_html=True)
+# st.markdown("""
+#     <style>
+#         #MainMenu {visibility: hidden;}
+#         footer {visibility: hidden;}
+#         header {visibility: hidden;}
+#     </style>
+# """, unsafe_allow_html=True)
 
