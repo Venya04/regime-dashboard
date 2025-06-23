e53a2b1ce2f2e6b65eae271948fd12ad2104fa13
Venya <154803459+Venya04@users.noreply.github.com>
2025-06-23 10:52:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -474,14 +474,14 @@ with left_col:
             fig_pie = px.pie(
                 names=list(filtered_alloc.keys()),
                 values=list(filtered_alloc.values()),
-                hole=0.3,
+                hole=0.5,
                 color=list(filtered_alloc.keys()),
                 color_discrete_map={
                     "stocks": "#023E8A",
-                    "stablecoins": "#005BC7",
-                    "cash": "#0049B8",
-                    "crypto": "#001499",
-                    "commodities": "#376CFE",
+                    "stablecoins": "#A8D1FA",
+                    "cash": "#87C2F7",
+                    "crypto": "#005BC7",
+                    "commodities": "#3B97F2",
                 }
             )
 
