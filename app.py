ab4505e59d3b609a3af4f2f046a3905dd21c6135
Venya <154803459+Venya04@users.noreply.github.com>
2025-06-23 11:09:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -543,7 +543,7 @@ with left_col:
             labels={"value": "Portfolio Value", "date": "Date"},
             template="plotly_dark",
             markers=True,
-            color_discrete_sequence=["#6e44ff"]
+            color_discrete_sequence=["#7161ef"]
         )
         perf_fig.update_traces(line=dict(width=3), marker=dict(size=6))
         perf_fig.update_layout(
