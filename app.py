7355c6123930d067ca4586e751e505bd43c4ebb0
Venya <154803459+Venya04@users.noreply.github.com>
2025-07-01 12:29:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ default_sections = {
 def get_sheet_client():
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     creds_dict = st.secrets["gcp_service_account"]
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(json.loads(json.dumps(creds_dict)), scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(dict(creds_dict), scope)
     client = gspread.authorize(creds)
     return client
 
